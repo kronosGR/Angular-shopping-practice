437aae7592766957dd816fc7d18ab367ac1e81d6
KronosGR <geo.elgeo@gmail.com>
2022-03-05 19:56:09 +0100

    more recipe-detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'This is a test desc',
       'https://www.mygreekdish.com/wp-content/uploads/2014/01/Traditional-Greek-bean-soup-recipe-Fasolada.jpg'
     ),
+    new Recipe(
+      'A test recipe',
+      'This is a test desc',
+      'https://www.mygreekdish.com/wp-content/uploads/2014/01/Traditional-Greek-bean-soup-recipe-Fasolada.jpg'
+    ),
   ];
 
   constructor() {}
