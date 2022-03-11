4b9b097cb2b95e8937d8105eaf0f91af8b4a71f5
KronosGR <geo.elgeo@gmail.com>
2022-03-11 20:00:03 +0100

    using shopping list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,17 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>()
+
   private recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
+      'A test recipe1',
       'This is a test desc',
       'https://www.mygreekdish.com/wp-content/uploads/2014/01/Traditional-Greek-bean-soup-recipe-Fasolada.jpg'
     ),
     new Recipe(
-      'A test recipe',
+      'A test recipe2',
       'This is a test desc',
       'https://www.mygreekdish.com/wp-content/uploads/2014/01/Traditional-Greek-bean-soup-recipe-Fasolada.jpg'
     ),
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -6,11 +6,15 @@ import { RecipeService } from './recipe.service';
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers: [RecipeService]
+  providers: [RecipeService],
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
+      this.selectedRecipe = recipe;
+    });
+  }
 }
