bdd19944bb6618590eb9f9a15218aeeeeaf041c9
KronosGR <geo.elgeo@gmail.com>
2022-03-12 19:40:19 +0100

    added ingredents to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,22 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>()
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
       'A test recipe1',
       'This is a test desc',
-      'https://www.mygreekdish.com/wp-content/uploads/2014/01/Traditional-Greek-bean-soup-recipe-Fasolada.jpg'
+      'https://www.mygreekdish.com/wp-content/uploads/2014/01/Traditional-Greek-bean-soup-recipe-Fasolada.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Fries', 11)]
     ),
     new Recipe(
       'A test recipe2',
       'This is a test desc',
-      'https://www.mygreekdish.com/wp-content/uploads/2014/01/Traditional-Greek-bean-soup-recipe-Fasolada.jpg'
+      'https://www.mygreekdish.com/wp-content/uploads/2014/01/Traditional-Greek-bean-soup-recipe-Fasolada.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Buns', 2)]
     ),
   ];
 
