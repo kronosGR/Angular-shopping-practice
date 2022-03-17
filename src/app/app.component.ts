479afca4e786342e8c6f0ba4fcafdf53a9bddf5a
KronosGR <geo.elgeo@gmail.com>
2022-03-17 20:00:37 +0100

    from eventemitter to observables and subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'recipe'
-  title = 'Angular-shopping-practice';
-
-  onNavigate(feature:string){
-    this.loadedFeature = feature
-  }
 }
