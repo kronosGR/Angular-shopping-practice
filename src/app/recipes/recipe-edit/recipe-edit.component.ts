a02d4c02dc2cf8f877239bc721b9a360c4522266
KronosGR <geo.elgeo@gmail.com>
2022-03-22 19:35:07 +0100

    syncing template with the form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -26,6 +26,9 @@ export class RecipeEditComponent implements OnInit {
       this.initForm();
     });
   }
+  onSubmit(){
+    console.log(this.recipeForm)
+  }
 
   private initForm() {
     let recipeName = '';
