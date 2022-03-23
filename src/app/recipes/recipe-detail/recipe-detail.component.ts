98190d3e904084b0c0ea8a3a00feef7e201542d6
KronosGR <geo.elgeo@gmail.com>
2022-03-23 06:49:42 +0100

    added cancel functionality

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -28,6 +28,10 @@ export class RecipeDetailComponent implements OnInit {
     this.router.navigate(['edit'], { relativeTo: this.route });
   }
 
+  onDeleteRecipe(){
+    this.recipeService.deleteRecipe(this.id)
+  }
+
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
