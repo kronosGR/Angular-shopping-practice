bcbf9c29e289af8ae8ad8c084d9a1ba94a7e6bd9
KronosGR <geo.elgeo@gmail.com>
2022-03-29 06:32:34 +0200

    added resolver

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,27 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+
+import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
-import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A test recipe1',
-      'This is a test desc',
-      'https://www.mygreekdish.com/wp-content/uploads/2014/01/Traditional-Greek-bean-soup-recipe-Fasolada.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('Fries', 11)]
-    ),
-    new Recipe(
-      'A test recipe2',
-      'This is a test desc',
-      'https://www.mygreekdish.com/wp-content/uploads/2014/01/Traditional-Greek-bean-soup-recipe-Fasolada.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('Buns', 2)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'A super-tasty Schnitzel - just awesome!',
+  //     'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'What else you need to say?',
+  //     'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+  //     [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+  //   )
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
@@ -35,7 +37,7 @@ export class RecipeService {
   }
 
   getRecipe(index: number) {
-    return this.recipes.slice()[index];
+    return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,33 +1,43 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
-import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(
-    private http: HttpClient,
-    private recipesService: RecipeService
-  ) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
   storeRecipes() {
-    const recipes = this.recipesService.getRecipes();
+    const recipes = this.recipeService.getRecipes();
     this.http
       .put(
         'https://ng-shopping-130e8-default-rtdb.firebaseio.com/recipes.json',
         recipes
       )
-      .subscribe((response) => console.log(response));
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>(
-        'https://ng-shopping-130e8-default-rtdb.firebaseio.com/recipes.json '
+        'https://ng-shopping-130e8-default-rtdb.firebaseio.com/recipes.json'
       )
-      .subscribe((recipes) => {
-        this.recipesService.setRecipes(recipes);
-      });
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
